50d4958d868fd7aa6e946638f3880a5f755fccf2
lidengfeng <412996906@qq.com>
2025-04-07 18:19:36 +0800

    代码修改

--- src/views/company/details/trade/base.vue
+++ src/views/company/details/trade/base.vue
@@ -6,6 +6,89 @@
                 <div class="item-content">XXXXXX单位</div>
             </div>
             <div class="table-item">
+                <div class="item-title">统一社会信用代码</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">机构类型</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">单位类型</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">主要经济指标（万元）</div>
+                <div class="item-list">
+                    <div class="line">
+                        <div class="item">
+                            <div><span>*</span><span>2024年营收:</span></div>
+                            <div>16800000</div>
+                        </div>
+                        <div class="item">
+                            <div><span>*</span><span>其中：主营业务收入</span></div>
+                            <div>16800000</div>
+                        </div>
+                    </div>
+                    <div class="line">
+                        <div class="item">
+                            <div><span>*</span><span>2024年资产总计:</span></div>
+                            <div>16800000</div>
+                        </div>
+                        <div class="item">
+                            <div><span>*</span><span>2024年营业利润：</span></div>
+                            <div>16800000</div>
+                        </div>
+                    </div>
+                    <div class="line">
+                        <div class="item">
+                            <div><span>*</span><span>2023年营收:</span></div>
+                            <div>16800000</div>
+                        </div>
+                    </div>
+                    <div class="line">
+                        <div class="item">
+                            <div><span>*</span><span>2022年营收:</span></div>
+                            <div>16800000</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="table-item-line">
+                <div class="line-item">
+                    <div class="item-title">人员数量</div>
+                    <div class="item-content">666</div>
+                </div>
+                <div class="line-item">
+                    <div class="item-title"><span> * </span><span>其中：女性（人）</span></div>
+                    <div class="item-content">123</div>
+                </div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">单位简介</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">通讯地址</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">详细地址</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">应用场景</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">是否开放共享数据</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">是否用于研发数据产品</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
                 <div class="item-title">填报人员</div>
                 <div class="table-box">
                     <div class="box-item">
@@ -47,6 +130,7 @@ export default {
 
 <style lang="scss" scoped>
 @import "~@/assets/styles/variables.scss";
+
 .app-container {
     .table-container {
         width: 100%;
@@ -87,6 +171,51 @@ export default {
                 box-sizing: border-box;
             }
 
+            .item-list {
+                width: calc(100% - #{$tableLeftTitleWidth} - 2px);
+                display: flex;
+                flex-direction: column;
+                padding: 6px 10px;
+                color: $tableTextColor;
+                box-sizing: border-box;
+
+                .line {
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
+                    color: $tableTextColor;
+                    box-sizing: border-box;
+
+                    .item {
+                        display: flex;
+                        align-items: center;
+                        font-size: 12px;
+                        color: $tableTextColor;
+                        box-sizing: border-box;
+
+                        div:nth-child(1) {
+                            width: 160px;
+                            padding: 6px 10px;
+                            margin-right: 6px;
+                            color: $tableTextColor2;
+                            font-weight: bold;
+                            background-color: $tableBgColor;
+
+                            span:nth-child(1) {
+                                display: inline-block;
+                                margin-right: 2px;
+                                color: red;
+                            }
+                        }
+
+                        div:nth-child(2) {
+                            width: 160px;
+                            margin-right: 20px;
+                        }
+                    }
+                }
+            }
+
             .table-box {
                 width: calc(100% - #{$tableLeftTitleWidth} - 2px);
                 display: flex;
@@ -108,6 +237,51 @@ export default {
                         color: $tableTextColor2;
                         font-weight: bold;
                     }
+                }
+            }
+        }
+
+        .table-item-line {
+            width: 100%;
+            display: flex;
+            border-bottom: 1px solid $tableBorderColor;
+
+            &:last-child {
+                border-bottom: none;
+            }
+
+            .line-item {
+                width: calc(100% / 2 - 2px);
+                display: flex;
+                border-right: 1px solid $tableBorderColor;
+
+                &:last-child {
+                    border-right: none;
+                }
+
+                .item-title {
+                    width: $tableLeftTitleWidth;
+                    display: flex;
+                    align-items: center;
+                    padding: 6px 10px;
+                    font-weight: 600;
+                    color: $tableTitleColor;
+                    border-right: 1px solid $tableBorderColor;
+                    background-color: $tableBgColor;
+                    box-sizing: border-box;
+                    span:nth-child(1) {
+                        display: inline-block;
+                        margin-right: 2px;
+                    }
+                }
+
+                .item-content {
+                    width: calc(100% - #{$tableLeftTitleWidth} - 2px);
+                    display: flex;
+                    align-items: center;
+                    padding: 6px 10px;
+                    color: $tableTextColor;
+                    box-sizing: border-box;
                 }
             }
         }
--- src/views/company/details/trade/table.vue
+++ src/views/company/details/trade/table.vue
@@ -1,6 +1,94 @@
 <template>
     <div class="app-container">
-        123
+        <div class="table-container">
+            <div class="header-container">
+                <div class="left-total">指标(至少一个行业)</div>
+                <div class="middle-box">
+                    <div class="middle">交易总额（万元）</div>
+                    <div class="little-list">
+                        <div>2024年</div>
+                        <div>2025年</div>
+                    </div>
+                </div>
+                <div class="middle-box">
+                    <div class="middle">交易次数（次）</div>
+                    <div class="little-list">
+                        <div>2024年</div>
+                        <div>2025年</div>
+                    </div>
+                </div>
+                <div class="large-box">
+                    <div class="large">数据集</div>
+                    <div class="middle-list">
+                        <div>可交易数量（个）</div>
+                        <div>交易数量（个）</div>
+                        <div>交易金额（万元）</div>
+                        <div>交易次数（次）</div>
+                        <div>省内购买方数量（个）</div>
+                        <div>省外购买方数量（个）</div>
+                        <div>省内供给方数量（个）</div>
+                        <div>省外供给方数量（个）</div>
+                    </div>
+                    <div class="little-list">
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                    </div>
+                </div>
+                <div class="large-box last">
+                    <div class="large">数据产品和服务</div>
+                    <div class="middle-list">
+                        <div>可交易数量（个）</div>
+                        <div>交易数量（个）</div>
+                        <div>交易金额（万元）</div>
+                        <div>交易次数（次）</div>
+                        <div>省内购买方数量（个）</div>
+                        <div>省外购买方数量（个）</div>
+                        <div>省内供给方数量（个）</div>
+                        <div>省外供给方数量（个）</div>
+                    </div>
+                    <div class="little-list">
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                        <div>2024年</div>
+                        <div>2025年</div>
+                    </div>
+                </div>
+            </div>
+            <div class="body-container">
+                <div class="body-list" v-for="(item, index) in list" :key="item.name">
+                    <div class="left-total">{{ item.name }}</div>
+                    <div class="body-item" v-for="(k, i) in item.value" :key="i">
+                        {{ k }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +98,52 @@ export default {
     name: 'TradeTable',
     data() {
         return {
+            list: [
+                {
+                    name: '农、林、牧、渔业',
+                    value: ['168', 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '采矿业',
+                    value: [168, '168123456', 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '制造业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '电力、热力、燃气及水生产和供应业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '建筑业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '批发和零售业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '交通运输、仓储和邮政业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '住宿和餐饮业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '信息传输、软件和信息技术服务业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '金融业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+                {
+                    name: '房地产业',
+                    value: [168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168, 168],
+                },
+            ]
         }
     },
     created() {
@@ -25,5 +159,163 @@ export default {
 .app-container {
     font-size: 12px;
     color: $tableTextColor;
+
+    .table-container {
+        width: 100%;
+        padding-right: 20px;
+        margin-right: 20px;
+        overflow-x: scroll;
+        overflow-y: hidden;
+        box-sizing: border-box;
+
+        .header-container {
+            height: calc(#{$bigTableCellHeight} * 3);
+            display: flex;
+            align-items: center;
+            box-sizing: border-box;
+
+            .left-total {
+                width: $bigTableTitleWidth;
+                height: calc(#{$bigTableCellHeight} * 3);
+                flex: 0 0 $bigTableTitleWidth;
+                line-height: calc(#{$bigTableCellHeight} * 3);
+                font-size: 12px;
+                font-weight: 600;
+                border: 1px solid $tableBorderColor;
+                border-right: none;
+                border-bottom: none;
+                text-align: center;
+                box-sizing: border-box;
+            }
+
+            .middle-box {
+                width: calc(#{$bigTableCellWidth} * 2);
+                height: calc(#{$bigTableCellHeight} * 3);
+                border: 1px solid $tableBorderColor;
+                border-right: none;
+                border-bottom: none;
+                box-sizing: border-box;
+
+                .middle {
+                    height: calc(#{$bigTableCellHeight} * 2);
+                    font-size: 12px;
+                    font-weight: 600;
+                    line-height: calc(#{$bigTableCellHeight} * 2);
+                    text-align: center;
+                    box-sizing: border-box;
+                }
+
+            }
+
+            .large-box {
+                height: calc(#{$bigTableCellHeight} * 3);
+                border: 1px solid $tableBorderColor;
+                border-right: none;
+                border-bottom: none;
+                box-sizing: border-box;
+
+                &.last {
+                    margin-left: -0.5px;
+                    border-right: 1px solid $tableBorderColor;
+                }
+
+                .large {
+                    height: calc(#{$bigTableCellHeight} * 1);
+                    font-size: 12px;
+                    font-weight: 600;
+                    line-height: calc(#{$bigTableCellHeight} * 1);
+                    border-bottom: 1px solid $tableBorderColor;
+                    text-align: center;
+                    box-sizing: border-box;
+                }
+
+                .middle-list {
+                    height: calc(#{$bigTableCellHeight} * 1);
+                    display: flex;
+                    align-items: center;
+                    box-sizing: border-box;
+
+                    div {
+                        width: calc(#{$bigTableCellWidth} * 2);
+                        height: calc(#{$bigTableCellHeight} * 1);
+                        line-height: calc(#{$bigTableCellHeight} * 1);
+                        font-size: 12px;
+                        font-weight: 600;
+                        border-right: 1px solid $tableBorderColor;
+                        text-align: center;
+                        box-sizing: border-box;
+
+                        &:last-child {
+                            border-right: none;
+                        }
+                    }
+                }
+            }
+
+            .little-list {
+                height: calc(#{$bigTableCellHeight} * 1);
+                display: flex;
+                align-items: center;
+                box-sizing: border-box;
+
+                div {
+                    width: calc(#{$bigTableCellWidth} * 1);
+                    height: calc(#{$bigTableCellHeight} * 1);
+                    line-height: calc(#{$bigTableCellHeight} * 1);
+                    font-size: 12px;
+                    font-weight: 600;
+                    border-top: 1px solid $tableBorderColor;
+                    border-right: 1px solid $tableBorderColor;
+                    text-align: center;
+                    box-sizing: border-box;
+
+                    &:last-child {
+                        border-right: none;
+                    }
+                }
+            }
+        }
+
+        .body-container {
+            width: fit-content;
+            margin-bottom: 20px;
+            border-bottom: 1px solid $tableBorderColor;
+            box-sizing: border-box;
+
+            .body-list {
+                // height: calc(#{$bigTableCellHeight} * 1);
+                height: fit-content;
+                display: flex;
+                align-items: center;
+                box-sizing: border-box;
+
+                .left-total {
+                    width: $bigTableTitleWidth;
+                    height: calc(#{$bigTableCellHeight} * 1);
+                    flex: 0 0 calc(#{$bigTableTitleWidth} + 1px);
+                    // margin-top: 0.5px;
+                    line-height: calc(#{$bigTableCellHeight} * 1);
+                    font-size: 12px;
+                    font-weight: 600;
+                    border: 1px solid $tableBorderColor;
+                    border-bottom: none;
+                    text-align: center;
+                    box-sizing: border-box;
+                }
+
+                .body-item {
+                    width: $bigTableCellWidth;
+                    height: calc(#{$bigTableCellHeight} * 1);
+                    flex: 0 0 $bigTableCellWidth;
+                    line-height: calc(#{$bigTableCellHeight} * 1);
+                    font-size: 12px;
+                    border-top: 1px solid $tableBorderColor;
+                    border-right: 1px solid $tableBorderColor;
+                    text-align: center;
+                    box-sizing: border-box;
+                }
+            }
+        }
+    }
 }
 </style>
