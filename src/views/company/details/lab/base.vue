8e4cb40defe213cc5da3d60ce60a1a0598544afb
lidengfeng <412996906@qq.com>
2025-04-07 20:45:28 +0800

    代码修改

--- src/views/company/details/lab/base.vue
+++ src/views/company/details/lab/base.vue
@@ -6,6 +6,48 @@
                 <div class="item-content">XXXXXX单位</div>
             </div>
             <div class="table-item">
+                <div class="item-title">统一社会信用代码</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">机构类型</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">单位类型</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item-line">
+                <div class="line-item">
+                    <div class="item-title">人员数量(截至2025.12)（人）</div>
+                    <div class="item-content">666</div>
+                </div>
+                <div class="line-item">
+                    <div class="item-title"><span> * </span><span>其中：女性（人）</span></div>
+                    <div class="item-content">123</div>
+                </div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">单位简介</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">通讯地址</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">详细地址</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">是否开放共享数据</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
+                <div class="item-title">是否用于研发数据产品</div>
+                <div class="item-content"></div>
+            </div>
+            <div class="table-item">
                 <div class="item-title">填报人员</div>
                 <div class="table-box">
                     <div class="box-item">
@@ -47,6 +89,7 @@ export default {
 
 <style lang="scss" scoped>
 @import "~@/assets/styles/variables.scss";
+
 .app-container {
     .table-container {
         width: 100%;
@@ -87,6 +130,51 @@ export default {
                 box-sizing: border-box;
             }
 
+            .item-list {
+                width: calc(100% - #{$tableLeftTitleWidth} - 2px);
+                display: flex;
+                flex-direction: column;
+                padding: 6px 10px;
+                color: $tableTextColor;
+                box-sizing: border-box;
+
+                .line {
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
+                    color: $tableTextColor;
+                    box-sizing: border-box;
+
+                    .item {
+                        display: flex;
+                        align-items: center;
+                        font-size: 12px;
+                        color: $tableTextColor;
+                        box-sizing: border-box;
+
+                        div:nth-child(1) {
+                            width: 160px;
+                            padding: 6px 10px;
+                            margin-right: 6px;
+                            color: $tableTextColor2;
+                            font-weight: bold;
+                            background-color: $tableBgColor;
+
+                            span:nth-child(1) {
+                                display: inline-block;
+                                margin-right: 2px;
+                                color: red;
+                            }
+                        }
+
+                        div:nth-child(2) {
+                            width: 160px;
+                            margin-right: 20px;
+                        }
+                    }
+                }
+            }
+
             .table-box {
                 width: calc(100% - #{$tableLeftTitleWidth} - 2px);
                 display: flex;
@@ -108,6 +196,51 @@ export default {
                         color: $tableTextColor2;
                         font-weight: bold;
                     }
+                }
+            }
+        }
+
+        .table-item-line {
+            width: 100%;
+            display: flex;
+            border-bottom: 1px solid $tableBorderColor;
+
+            &:last-child {
+                border-bottom: none;
+            }
+
+            .line-item {
+                width: calc(100% / 2 - 2px);
+                display: flex;
+                border-right: 1px solid $tableBorderColor;
+
+                &:last-child {
+                    border-right: none;
+                }
+
+                .item-title {
+                    width: $tableLeftTitleWidth;
+                    display: flex;
+                    align-items: center;
+                    padding: 6px 10px;
+                    font-weight: 600;
+                    color: $tableTitleColor;
+                    border-right: 1px solid $tableBorderColor;
+                    background-color: $tableBgColor;
+                    box-sizing: border-box;
+                    span:nth-child(1) {
+                        display: inline-block;
+                        margin-right: 2px;
+                    }
+                }
+
+                .item-content {
+                    width: calc(100% - #{$tableLeftTitleWidth} - 2px);
+                    display: flex;
+                    align-items: center;
+                    padding: 6px 10px;
+                    color: $tableTextColor;
+                    box-sizing: border-box;
                 }
             }
         }
--- src/views/company/details/lab/table.vue
+++ src/views/company/details/lab/table.vue
@@ -1,6 +1,121 @@
 <template>
     <div class="app-container">
-        123
+        <div class="header-container">
+            <div class="left-total">指标</div>
+            <div class="right">累计值（截至2025年底）</div>
+            <div class="right">年度值（2024年）</div>
+            <div class="right">年度值（2025年）</div>
+        </div>
+        <div class="item-container">
+            <div class="right-box">
+                <div class="item">
+                    <div class="item-title">数据生产总量（TB）</div>
+                    <div class="right-item">1</div>
+                    <div class="right-item">1</div>
+                    <div class="right-item">1</div>
+                </div>
+                <div class="item">
+                    <div class="item-title">数据存储总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title third">原始数据存储总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title third">衍生数据存储总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title third">备份数据存储总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title third">结构化数据存储总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">云存储总量(TB)</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">数据边缘设备存储总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">数据终端设备存储总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">冷数据占比（%）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">开放数据集总数（个）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">开放数据总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">自建算力规模（TFLOPS）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">租用算力规模（TFLOPS）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">自建大模型数量（个）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">AI投入金额（万元）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+                <div class="item">
+                    <div class="item-title">用于AI计算的数据集数据总量（TB）</div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                    <div class="right-item"></div>
+                </div>
+            </div>
+        </div>
+        <div class="notes-container">
+            <div class="left">备注</div>
+            <div class="right"></div>
+        </div>
     </div>
 </template>
 
@@ -25,5 +140,130 @@ export default {
 .app-container {
     font-size: 12px;
     color: $tableTextColor;
+
+    .header-container {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        color: $tableTitleColor;
+        font-weight: 600;
+        background-color: $tableBgColor;
+
+        div {
+            padding: 7px 10px;
+            text-align: center;
+            border: 1px solid $tableBorderColor;
+            border-right: none;
+            box-sizing: border-box;
+
+            &:last-child {
+                border-right: 1px solid $tableBorderColor;
+            }
+        }
+
+        .left-total {
+            width: $tableLeftTitleWidth3;
+            text-align: center;
+        }
+
+        .right {
+            width: calc((100% - #{$tableLeftTitleWidth3}) / 3);
+        }
+    }
+
+    .item-container {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        border: 1px solid $tableBorderColor;
+        border-top: none;
+        background-color: $tableBgColor;
+        box-sizing: border-box;
+
+        .right-box {
+            width: 100%;
+            box-sizing: border-box;
+
+            .item {
+                width: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: stretch;
+                box-sizing: border-box;
+
+                .item-title {
+                    display: flex;
+                    align-items: center;
+                    width: $tableLeftTitleWidth3;
+                    padding: 7px 10px;
+                    color: $tableTitleColor;
+                    font-weight: 600;
+                    text-align: left;
+                    border-left: 1px solid $tableBorderColor;
+                    border-bottom: 1px solid $tableBorderColor;
+                    box-sizing: border-box;
+
+                    &.third {
+                        padding-left: 20px;
+                        font-weight: 500;
+                    }
+                }
+
+                .right-item {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    width: calc((100% - #{$tableLeftTitleWidth3}) / 3);
+                    height: 1;
+                    padding: 7px 10px;
+                    border-left: 1px solid $tableBorderColor;
+                    border-bottom: 1px solid $tableBorderColor;
+                    background-color: #fff;
+                    box-sizing: border-box;
+                }
+
+                &:last-child {
+                    .item-title {
+                        border-bottom: none;
+                    }
+
+                    .right-item {
+                        border-bottom: none;
+                    }
+                }
+            }
+        }
+    }
+
+    .notes-container {
+        display: flex;
+        align-items: stretch;
+
+        .left {
+            width: calc(#{$tableLeftTitleWidth3} + 1px);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 7px 10px;
+            color: $tableTitleColor;
+            font-weight: 600;
+            border: 1px solid $tableBorderColor;
+            border-top: none;
+            border-right: none;
+            background-color: $tableBgColor;
+            box-sizing: border-box;
+        }
+
+        .right {
+            flex: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 7px 10px;
+            border: 1px solid $tableBorderColor;
+            border-top: none;
+            box-sizing: border-box;
+        }
+    }
 }
 </style>
